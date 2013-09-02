7aeeccf370ea90f537fb23f879bba02a9e37e445
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-02 16:11:27 -0500

    added migration for users count column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130902183756) do
+ActiveRecord::Schema.define(:version => 20130902203909) do
 
   create_table "karma_points", :force => true do |t|
     t.integer  "user_id",    :null => false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20130902183756) do
     t.string   "email",                    :null => false
     t.datetime "created_at",               :null => false
     t.datetime "updated_at",               :null => false
+    t.integer  "count"
   end
 
 end
---
+++ lib/tasks/populate.rake
@@ -0,0 +1,6 @@
+namespace :db do
+  desc "populate count field with total karma points"
+  task :populate do
+    #still working on it
+  end
+end
